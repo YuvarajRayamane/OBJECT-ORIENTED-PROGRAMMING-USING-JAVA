715c6c69bcb37aa32fc4f99be84d06ddba257c40
Yuvaraj Rayamane <102849877+YuvarajRayamane@users.noreply.github.com>
2022-09-01 19:54:25 +0530

    Create iii_SubString.java
    iii) Write a program that splits a string into a number of substrings with the help of string split() method and then prints the substrings.

---
+++ 10_String_Handling/iii_SubString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SubString {
+    public static void main(String[] args) {
+        System.out.println("Enter the sentence: ");
+        Scanner in = new Scanner(System.in);
+        String inp = in.nextLine();
+        System.out.println("Enter the character/s by which the sentence must be divided to substrings: ");
+        String ch = in.next();
+        String[] subs = inp.split(ch);
+        System.out.println("The resulting substrings are: ");
+        for (String a : subs)
+            System.out.print(a + " ");
+    }
+}
